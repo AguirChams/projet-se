e26a591e15f8ec5fb432947f656f5fc1f23f0c19
AguirChams <aguirchams68@gmail.com>
2025-05-06 18:09:58 +0200

    added commands

--- install_env.sh
+++ install_env.sh
@@ -6,3 +6,19 @@
  docker-compose up -d
  docker ps
  docker exec -it mariadb bash
+
+ # creation bd
+  mariadb -u user -p 
+  userpass
+  USE projetdb
+  CREATE TABLE users ( 
+id INT AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR(100)
+);
+INSERT INTO users (nom)
+ VALUES ('CHAMS'), ('YAHYA'), ('YASSINE'), ('ABDELLAH
+'), ('ASMAE');
+
+# monitoring des containers
+ docker stats
+ docker top mariadb
