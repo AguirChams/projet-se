3f606071698dd374847a9964e61a3a0eda6f31ad
AguirChams <aguirchams68@gmail.com>
2025-05-06 17:59:27 +0200

    push

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `user` (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nom VARCHAR(100)
+);
+
+INSERT INTO `user` (nom) VALUES 
+('John Doe'), 
+('Jane Smith'), 
+('Alice Johnson'), 
+('Bob Brown'), 
+('Charlie Black');
---
+++ install_env.sh
@@ -0,0 +1,8 @@
+
+
+
+#les commandes a entrer
+
+ docker-compose up -d
+ docker ps
+ docker exec -it mariadb bash
